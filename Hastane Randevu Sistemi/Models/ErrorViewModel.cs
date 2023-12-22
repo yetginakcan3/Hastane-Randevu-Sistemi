5e77bc66a33a336be37ccb9a06d4110d312a866b
yetginakcan3 <yetginakcan3@gmail.com>
2023-12-22 17:59:03 +0300

    Add project files.

---
+++ Hastane Randevu Sistemi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hastane_Randevu_Sistemi.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
